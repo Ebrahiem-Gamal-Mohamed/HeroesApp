d35465c3d21cf1b424ced7856cde52ca2837095f
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2020-03-27 03:46:27 +0200

    enhance hero profile

--- src/app/heroes/hero-profile/hero-profile.component.ts
+++ src/app/heroes/hero-profile/hero-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IHero } from './../hero.model';
 import { HeroService } from './../hero.service';
 import { ActivatedRoute } from '@angular/router';
+import { FastAccessService } from './../../shared/_components/fast-access/fast-access.service';
 
 @Component({
   selector: 'app-hero-profile',
@@ -13,8 +14,11 @@ export class HeroProfileComponent implements OnInit {
   hero: IHero;
   heroId: number;
 
-  constructor(private heroService: HeroService, private route: ActivatedRoute) {
-      this.heroId = +this.route.snapshot.paramMap.get('id');
+  constructor(
+    private heroService: HeroService, 
+    private route: ActivatedRoute, 
+    public fastAccessService: FastAccessService) {
+    this.heroId = +this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
