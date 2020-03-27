d900cae707959f5dd7508f1efe41db9e2ece7bb8
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2020-03-27 03:45:39 +0200

    add primeng modal and swither and animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 // thirdParties
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { DialogModule } from 'primeng/dialog';
+
 // components ...
 import { AppComponent } from './app.component';
 import { HeroesListComponent } from './heroes/heroes-list/heroes-list.component';
 import { HeroProfileComponent } from './heroes/hero-profile/hero-profile.component';
+import { EmptyComponent } from './shared/_components/empty/empty.component';
+import { FastAccessComponent } from './shared/_components/fast-access/fast-access.component';
 
 import { NgbdSortableHeader } from './shared/_directives/sortable.directive';
-import { EmptyComponent } from './shared/_components/empty/empty.component';
 
 @NgModule({
   declarations: [
@@ -19,16 +26,21 @@ import { EmptyComponent } from './shared/_components/empty/empty.component';
     HeroesListComponent,
     HeroProfileComponent,
     NgbdSortableHeader,
-    EmptyComponent
+    EmptyComponent,
+    FastAccessComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    InputSwitchModule,
+    DialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [FastAccessComponent]
 })
 export class AppModule { }
